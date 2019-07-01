6a456747317fc5ffb1f9ebac7e973093eb2d40ad
Eckstein, Jenny (CORP) <Jenny.Eckstein@adp.com>
2019-06-30 20:02:29 -0400

    progress

--- client/index.js
+++ client/index.js
@@ -3,14 +3,25 @@ const FormData = require('form-data');
 const concat = require("concat-stream")
 const axios = require('axios');
 const fs = require('fs');
+const request = require("request");
+var wstream = fs.createWriteStream('./test.md');
 
 async function onRequest(req, res) {
   try {
-    // console.log("JENNY RES: ", req);
+    console.log("JENNY req.body: ", req);
+    // req.pipe(wstream);
+    req
+    .pipe(request.post('http://localhost:3002/'))
+    .pipe(res);
+
+    req.on('end', () => {
+      console.log('END?');
+    });
+    return;
     const fd = new FormData();
     // fd.append('jennyFile', './README.md');
-    fd.append("jennyFile", fs.createReadStream('./README.md'));
-    fd.append("jennyFile1", fs.createReadStream('./README1.md'));
+    // fd.append("jennyFile", fs.createReadStream('./README.md'));
+    // fd.append("jennyFile1", fs.createReadStream('./README1.md'));
     // const config = { headers: { 'Content-Type': 'multipart/form-data' } };
 
     // const b64 = base64Img.base64Sync('./test.jpg').split('base64,')[1]
@@ -20,7 +31,7 @@ async function onRequest(req, res) {
 
     fd.pipe(concat({ encoding: 'buffer' }, async data => {
       const request = await axios.post(
-        'http://localhost:3001',
+        'http://localhost:3002',
         data,
         {
           headers: fd.getHeaders()
@@ -36,4 +47,4 @@ async function onRequest(req, res) {
 }
 
 
-http.createServer(onRequest).listen(3000);
+http.createServer(onRequest).listen(3010);
--- server/index.js
+++ server/index.js
@@ -1,26 +1,29 @@
 const http = require("http");
 var fs = require('fs');  // file system
-var rstream = fs.createWriteStream('./test.md');
+var rstream = fs.createReadStream('./test.md');
+var wstream = fs.createWriteStream('./test.md');
 
 function onRequest(req, res) {
+  var dataLength;
   console.log("JENNY INSIDE SERVER");
-  console.log(Object.keys(req));
-  console.log(req.headers);
-  console.log("---------")
-  console.log("files: ", req.files);
-  console.log("body: ", req.body);
+  // console.log(Object.keys(req));
+  console.log(req);
+  // console.log("---------")
+  // console.log("files: ", req.files);
+  // console.log("body: ", req.body);
   // console.log("file: ", req.connection);
-  res.write("success")
+  // res.write("success")
   
 
-  // rstream
-  // .on('data', function (chunk) {
-  //   dataLength += chunk.length;
-  // })
-  // .on('end', function () {  // done
-  //   console.log('The length was:', dataLength);
-  // });
-  // res.end();
+  rstream
+  .on('data', function (chunk) {
+    dataLength += chunk.length;
+    console.log("JENNY DATA: ", chunk);
+  })
+  .on('end', function () {  // done
+    console.log('The length was:', dataLength);
+  });
+  res.end();
 }
 
-http.createServer(onRequest).listen(3001);
+http.createServer(onRequest).listen(3002);
